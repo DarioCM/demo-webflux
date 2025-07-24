bba3223c7b39d805961e5d3fe2cc8f0d358842d5
Carlos Castañeda <dario20049@gmail.com>
2025-07-24 19:24:43 +0900

    feat: add employee entity document

---
+++ src/main/java/dev/dario/webflux/employee/Employee.java
@@ -0,0 +1,24 @@
+package dev.dario.webflux.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collation = "employees")
+public class Employee {
+
+  @Id
+  private String id;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+
+}
