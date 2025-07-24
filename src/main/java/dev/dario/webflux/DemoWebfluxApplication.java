c16573a75b1f199bda5bb9c79c937e5e16111967
Carlos Castañeda <dario20049@gmail.com>
2025-07-24 19:13:52 +0900

    mongo db uri

--- src/main/java/dev/dario/webflux/DemoWebfluxApplication.java
+++ src/main/java/dev/dario/webflux/DemoWebfluxApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoWebfluxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoWebfluxApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoWebfluxApplication.class, args);
+  }
 
 }
