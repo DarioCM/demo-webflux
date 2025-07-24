5b88b7696b74d245a42839766acac004caa8785a
Carlos Castañeda <dario20049@gmail.com>
2025-07-24 23:35:41 +0900

    feat: doesnt use lombok

--- src/main/java/dev/dario/webflux/employee/EmployeeDto.java
+++ src/main/java/dev/dario/webflux/employee/EmployeeDto.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class EmployeeDto {
 
   private String id;
@@ -17,4 +14,45 @@ public class EmployeeDto {
   private String lastName;
   private String email;
 
+  public EmployeeDto() {
+  }
+
+  public EmployeeDto(String id, String firstName, String lastName, String email) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
