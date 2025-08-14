679a679546c1b81d7bc4af5ca48b08b8dc101da2
Carlos Castañeda <dario20049@gmail.com>
2025-08-14 19:59:10 +0900

    feat: test integration

--- src/test/java/dev/dario/webflux/employee/EmployeeControllerIntegrationTest.java
+++ src/test/java/dev/dario/webflux/employee/EmployeeControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package dev.dario.webflux.employee;
 
+import java.util.Collections;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,10 +19,18 @@ class EmployeeControllerIntegrationTest {
   private EmployeeService service;
 
   @Autowired
+  private EmployeeRepository repository;
+
+  @Autowired
   private WebTestClient webTestClient;
 
+  @BeforeEach
+  void before(){
+    repository.deleteAll().subscribe();
+  }
+
   @Test
-  void testSaveEmployee(){
+  void testSaveEmployee() {
 
     EmployeeDto employeeDto = new EmployeeDto();
     employeeDto.setFirstName("casta");
@@ -39,4 +49,99 @@ class EmployeeControllerIntegrationTest {
         .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
         .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
   }
+
+  @Test
+  void testGetSingleEmployee() {
+
+    EmployeeDto employeeDto = new EmployeeDto();
+    employeeDto.setFirstName("casta");
+    employeeDto.setLastName("carlos");
+    employeeDto.setEmail("dario20049@gmail.com");
+
+    EmployeeDto savedEmployee = service.saveEmployee(employeeDto).block();
+
+    webTestClient.get()
+        .uri("/api/employees/{id}", Collections.singletonMap("id", savedEmployee.getId()))
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody()
+        .consumeWith(System.out::println)
+        .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+        .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+        .jsonPath("$.email").isEqualTo(employeeDto.getEmail())
+        .jsonPath("$.id").isEqualTo(savedEmployee.getId());
+
+  }
+
+  @Test
+  void testGetAllEmployees() {
+
+    EmployeeDto employeeDto = new EmployeeDto();
+    employeeDto.setFirstName("casta");
+    employeeDto.setLastName("carlos");
+    employeeDto.setEmail("dario20049@gmail.com");
+    service.saveEmployee(employeeDto).block();
+
+    EmployeeDto employeeDto2 = new EmployeeDto();
+    employeeDto2.setFirstName("casta2");
+    employeeDto2.setLastName("carlos2");
+    employeeDto2.setEmail("dario20049@gmail.com");
+    service.saveEmployee(employeeDto2).block();
+
+
+    webTestClient.get().uri("/api/employees")
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBodyList(EmployeeDto.class)
+        .consumeWith(System.out::println);
+  }
+
+  @Test
+  void testUpdateEmployee(){
+
+    EmployeeDto employeeDto = new EmployeeDto();
+    employeeDto.setFirstName("casta");
+    employeeDto.setLastName("carlos");
+    employeeDto.setEmail("dario20049@gmail.com");
+    EmployeeDto savedEmployee = service.saveEmployee(employeeDto).block();
+
+    EmployeeDto updateEmployee = new EmployeeDto();
+    updateEmployee.setFirstName("mendoza");
+    updateEmployee.setLastName("dario");
+    updateEmployee.setEmail("change9@gmail.com");
+
+    webTestClient.put().uri("/api/employees/{id}", Collections.singletonMap("id", savedEmployee.getId()))
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .body(Mono.just(updateEmployee), EmployeeDto.class)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody()
+        .consumeWith(System.out::println)
+        .jsonPath("$.firstName").isEqualTo(updateEmployee.getFirstName())
+        .jsonPath("$.lastName").isEqualTo(updateEmployee.getLastName())
+        .jsonPath("$.email").isEqualTo(updateEmployee.getEmail());
+
+  }
+
+  @Test
+  void testDeleteEmployee(){
+
+    EmployeeDto employeeDto = new EmployeeDto();
+    employeeDto.setFirstName("casta");
+    employeeDto.setLastName("carlos");
+    employeeDto.setEmail("dario20049@gmail.com");
+    EmployeeDto savedEmployee = service.saveEmployee(employeeDto).block();
+
+    webTestClient.delete().uri("/api/employees/{id}", Collections.singletonMap("id", savedEmployee.getId()))
+        .exchange()
+        .expectStatus().isNoContent()
+        .expectBody()
+        .consumeWith(System.out::println);
+
+
+
+  }
+
 }
